19042b02455868f8ef90e2de4bbc89ccef659fb0
Khalil Dhoiybi <kkhalildoiybi@gmail.com>
2022-09-28 10:50:21 +0100

    Item preview img bugfix

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -29,6 +29,8 @@ const ItemPreview = (props) => {
     }
   };
 
+  const placeholderImg = process.env.PUBLIC_URL + "/placeholder.png";
+
   return (
     <div
       className="card bg-dark border-light p-3"
@@ -36,7 +38,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image}
+        src={item.image === "" ? placeholderImg : item.image}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
