6e90d62645edfb6395913f69d5329c954326bba9
KhalilDhoiybi <kkhalildoiybi@gmail.com>
2022-09-30 11:51:07 +0100

    makes get word render searchbar

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -4,6 +4,7 @@ import logo from "../../imgs/logo.png";
 
 const Banner = (props) => {
   const [title, setTitle] = useState("");
+  const [toggleSearch, setToggleSearch] = useState(false);
 
   useEffect(() => {
     if (title.length >= 3 || title === "") {
@@ -23,44 +24,60 @@ const Banner = (props) => {
     setTitle(ev.target.value);
   };
 
+  const SearchBar = () => {
+    return (
+      <div className="col-5">
+        <div className="input-group">
+          <input
+            style={{
+              outline: "0 !important",
+              borderColor: "initial",
+              boxShadow: "none",
+            }}
+            id="search-box"
+            className="form-control form-control-lg py-2 border-right-0 border"
+            type="search"
+            placeholder="What is it that you truly desire?"
+            onChange={(ev) => {
+              onChangeHandle(ev);
+            }}
+            value={title}
+          />
+          <span className="input-group-append">
+            <i
+              className="bi bi-search form-control form-control-lg py-2 border-left-0"
+              style={{
+                borderTopLeftRadius: "0",
+                borderBottomLeftRadius: "0",
+                color: "#8671c7",
+                fontWeight: "bold",
+              }}
+            ></i>
+          </span>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div className="row">
           <div className="col p-0 d-flex flex-row-reverse align-items-center">
-            <span id="get-part">A place to get</span>
-          </div>
-          <div className="col-5">
-            <div className="input-group">
-              <input
-                style={{
-                  outline: "0 !important",
-                  borderColor: "initial",
-                  boxShadow: "none",
+            <span id="get-part">
+              A place to{" "}
+              <span
+                className="user-select-none"
+                onClick={() => {
+                  setToggleSearch(!toggleSearch);
                 }}
-                id="search-box"
-                className="form-control form-control-lg py-2 border-right-0 border"
-                type="search"
-                placeholder="What is it that you truly desire?"
-                onChange={(ev) => {
-                  onChangeHandle(ev);
-                }}
-                value={title}
-              />
-              <span className="input-group-append">
-                <i
-                  className="bi bi-search form-control form-control-lg py-2 border-left-0"
-                  style={{
-                    borderTopLeftRadius: "0",
-                    borderBottomLeftRadius: "0",
-                    color: "#8671c7",
-                    fontWeight: "bold",
-                  }}
-                ></i>
+              >
+                get
               </span>
-            </div>
+            </span>
           </div>
+          {toggleSearch ? <SearchBar /> : <span>&nbsp;</span>}
           <div className="col p-0 d-flex flex-row align-items-center">
             <span>the cool stuff.</span>
           </div>
